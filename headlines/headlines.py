9c0b807186eb9b3be964ffdfdabc52f6ab5dee27
dead911 <dead911@126.com>
2018-05-20 13:09:40 +0800

    Netease rss added

--- headlines/headlines.py
+++ headlines/headlines.py
@@ -1,20 +1,28 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 
 import feedparser
 
 app = Flask(__name__, template_folder='templates')
 
-BBC_FEED = {'bbc':"http://feeds.bbci.co.uk/news/rss.xml",
-            'cnn':"http://rss.cnn.com/rss/edition.rss",
-            'fox':"http://feeds.foxnews.com/foxnews/latest",
-            'iol':"http://www.iol.co.za/cmlink/1.640"}
+RSS_FEEDS = {'bbc':"http://feeds.bbci.co.uk/news/rss.xml",
+             'cnn':"http://rss.cnn.com/rss/edition.rss",
+             'fox':"http://feeds.foxnews.com/foxnews/latest",
+             'iol':"http://www.iol.co.za/cmlink/1.640",
+             'netease':"http://news.163.com/special/00011K6L/rss_newstop.xml"}
 
 @app.route("/")
-@app.route("/<publication>")
-def get_news(publication="bbc"):
-    feed = feedparser.parse(BBC_FEED[publication])
-    return render_template("home.html",articles=feed['entries'])
+def get_news():
+    query = request.args.get("publication")
+    if not query or query.lower() not in RSS_FEEDS:
+        publication = "bbc"
+    else:
+        publication = query.lower()
+
+    feed = feedparser.parse(RSS_FEEDS[publication])
+    articles = feed['entries']
+    return render_template("home.html",articles=articles)
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)

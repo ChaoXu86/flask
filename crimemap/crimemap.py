a061370819a22db5839bb5d5220019b703941733
dead911 <dead911@126.com>
2018-06-03 18:03:49 +0800

    add new crimemap functionality

--- crimemap/crimemap.py
+++ crimemap/crimemap.py
@@ -1,4 +1,6 @@
 import dbconfig
+import json
+
 if dbconfig.test:
     from mockdbhelper import MockDBHelper as DBHelper
 else:
@@ -12,12 +14,19 @@ DB = DBHelper()
 
 @app.route("/")
 def home():
-    try:
-        data = DB.get_all_inputs()
-    except Exception as e:
-        print(e)
-        data = None
-    return render_template("home.html", data = data)
+    crimes = DB.get_all_crimes()
+    crimes = json.dumps(crimes)
+    return render_template("home.html", crimes = crimes)
+
+@app.route("/submitcrime", methods=['POST'])
+def submitcrime():
+    category = request.form.get("category")
+    date = request.form.get("date")
+    latitude = float(request.form.get("latitude"))
+    longitude = float(request.form.get("longitude"))
+    description = request.form.get("description")
+    DB.add_crime(category, date, latitude, longitude, description)
+    return home()
 
 @app.route("/add", methods=["POST"])
 def add():
